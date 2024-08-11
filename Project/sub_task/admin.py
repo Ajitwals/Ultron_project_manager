1a7f0de5a836f74b62bb3d34089eee36adfb6a5c
ultron <ajitwalvekarshield@gmail.com>
2024-08-11 20:06:41 +0530

    "made a project management system"

--- Project/sub_task/admin.py
+++ Project/sub_task/admin.py
@@ -7,12 +7,6 @@ class Employee_data(admin.ModelAdmin):
     search_fields = ('name_of_employee',)
 admin.site.register(Employee,Employee_data)
 
-class Project_data(admin.ModelAdmin):
-    list_display = ('name_of_project','description','date_created')
-    search_fields = ('name_of_project',)
-    list_per_page = 5
-admin.site.register(Project,Project_data)
-
 
 class Project_time(admin.ModelAdmin):
     list_display = ('project_name','project_deadline',)
@@ -20,8 +14,17 @@ class Project_time(admin.ModelAdmin):
 
 admin.site.register(Project_timelines,Project_time)
 
+class Project_data(admin.ModelAdmin):
+    list_display = ('name_of_project','description','date_created')
+    search_fields = ('name_of_project',)
+    list_per_page = 5
+admin.site.register(Project,Project_data)
+
+
+
 class Task_data(admin.ModelAdmin):
     list_display = ('task_title','Name_of_project','task_created_on','task_started','task_in_progress','task_completed',)
     search_fields = ('task_title',)
+    list_filter = ('task_completed','task_started')
     list_per_page = 5
 admin.site.register(Task,Task_data)
